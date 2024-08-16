3a63b5b0343d68ff505c0b8b9024ef4b9f983a9c
santhoshbabu0411 <santhoshbabu0411@gmail.com>
2024-08-16 23:15:06 +0530

    Updated header.js

--- header.js
+++ header.js
@@ -1,8 +1,3 @@
 <script>
 document.getElementById("demo").innerHTML = "My First JavaScript";
-<<<<<<< HEAD
-console.log ('its a local changes')
-=======
-console.log ('its a Remote changes')
->>>>>>> 74b9787c389e0339f1cf761e471d378e87b00ff5
 </script>
