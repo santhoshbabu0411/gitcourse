3eba75de5b08023e9400600883ed6e6dcd0b8c1a
santhoshbabu0411 <96950191+santhoshbabu0411@users.noreply.github.com>
2024-08-16 22:50:36 +0530

    Update header.js
    Removed the remote header.js file

--- header.js
+++ header.js
@@ -1,9 +1,4 @@
 <script>
-<<<<<<< HEAD
 document.getElementById("demo").innerHTML = "My First Javascript";
 console.log('its a local changes')
-=======
-document.getElementById("demo").innerHTML = "My First JavaScript";
-console.log('its a Remote changes')
->>>>>>> 839e5e5123c1c21451f1552a13cb49b933e8afc7
 </script>
